c8311c64fc15bcf89e88e9bea4c55ae4790c697e
Hassan Raja <hassan.uog001@gmail.com>
2019-09-07 11:20:27 +0500

    can add number of quotes (1 to 5) to display

--- script.js
+++ script.js
@@ -26,14 +26,45 @@ let quotes = {
 };
 let min = 0;
 let max = 5;
-let quoteEl = document.createElement("p");
+let numberOfQuotes = 1;
 let generateQuoteBtn = document.querySelector("#generate-quote");
 let quotesList = document.querySelector(".quotes__list");
+let quotesListContainer = document.querySelector(".quotes__container");
+/// generateRandomNumber
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+const generateQuote = () => {
+  let quoteEl = document.createElement("li");
+  quoteEl.innerHTML = `${quotes.beginning[generateRandomNumber()]}  ${
+    quotes.middle[generateRandomNumber()]
+  } ${quotes.end[generateRandomNumber()]}`;
+  return quoteEl;
+};
+/// Function to generate quotes by joining three sentence fragments
+const getQuotes = numberOfQuotes => {
+  console.log(numberOfQuotes);
+  // if(numberOfQuotes === 1) {
+  //   quoteEl = `${quotes.beginning[generateRandomNumber()]}  ${
+  //     quotes.middle[generateRandomNumber()]
+  //     } ${quotes.end[generateRandomNumber()]}`;
+  //   quotesList.appendChild(quoteEl);
+  //   return quotesList;
+  // }
+  // if (numberOfQuotes > 1) {
+  for (let i = 0; i < numberOfQuotes; i++) {
+    quotesList.appendChild(generateQuote());
+  }
+  return quotesList;
+  // }
+};
+let dropdown = document.querySelector("#quotesCount");
+dropdown.addEventListener("change", e => {
+  numberOfQuotes = e.target.value;
+});
+
+/// Button to generate quotes on click
 generateQuoteBtn.addEventListener("click", () => {
-  let randomNumBegin = Math.floor(Math.random() * (max - min + 1)) + min;
-  let randomNumMiddle = Math.floor(Math.random() * (max - min + 1)) + min;
-  let randomNumEnd = Math.floor(Math.random() * (max - min + 1)) + min;
-  let quote = `${quotes.beginning[randomNumBegin]}  ${quotes.middle[randomNumMiddle]} ${quotes.end[randomNumEnd]}`;
-  quoteEl = quote;
-  quotesList.innerHTML = quoteEl;
+  quotesList.innerHTML = "";
+  quotesListContainer.appendChild(getQuotes(numberOfQuotes));
 });
